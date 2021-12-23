34d7ef4dd8748b6ad2ca6726644445ce80195e91
0xdevelopereth <0xdevelopereth@gmail.com>
2021-12-23 23:41:19 +0100

    :hammer: readjust order og sidebar links

--- src/navs/documentation.js
+++ src/navs/documentation.js
@@ -7,20 +7,20 @@ const pages = createPageList(
 
 export const documentationNav = {
   'Getting Started': [pages['welcome'], pages['faq']],
-  'BYO Ecosystem': [
+  Ecosystem: [
     pages['byopills'],
     pages['apostles'],
     pages['byoland'],
-    pages['byocraft'],
     pages['byokey'],
     pages['byovape'],
+    pages['byocraft'],
     pages['tryp'],
   ],
+  'Community & Support': [pages['contribute'], pages['policies'], pages['support']],
   Partnership: [
     pages['galaxy-fight-club'],
     pages['chibi-clash'],
     pages['the-red-village'],
     pages['legends-of-venari'],
   ],
-  'Community & Support': [pages['contributing'], pages['support'], pages['policies']],
 }
