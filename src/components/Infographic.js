a6077830e2af4c66a5c0c58bd8f47d5e49a84712
0xdevelopereth <0xdevelopereth@gmail.com>
2021-12-27 00:33:18 +0100

    :hammer: wording of component

--- src/components/Infographic.js
+++ src/components/Infographic.js
@@ -1,6 +1,6 @@
 import { MapInteractionCSS } from 'react-map-interaction'
 
-export function infographic(img) {
+export function Infographic(img) {
   return (
     <MapInteractionCSS showControls="true" maxScale="5" minScale="1">
       <img alt={img.alt} src={require(`../img/${img.src}`).default} />
