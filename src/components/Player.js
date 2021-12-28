46b952dc8af90f72ca8572ac250cfb39f88624b6
0xdevelopereth <0xdevelopereth@gmail.com>
2021-12-28 17:26:24 +0100

    :hammer: center video and mute props

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,25 +2,27 @@ import ReactPlayer from 'react-player'
 import clsx from 'clsx'
 
 export function Player(props) {
-  const { src, noSpacing = false, external = false, className } = props
+  const { src, noSpacing = false, external = false, className, muted = false } = props
 
   return (
     <div
-      className={clsx('group flex justify-center', className, {
+      className={clsx('group flex justify-center', {
         'pb-8': external,
       })}
     >
-      <ReactPlayer
-        className={clsx('', {
-          'mt-[-2rem]': noSpacing,
-        })}
-        loop
-        controls
-        url={external ? src : require(`@/video/${src}`).default}
-        width="100%"
-        height={external ? '423px' : '100%'}
-        volume={0.3}
-      />
+      <div className={clsx(className)}>
+        <ReactPlayer
+          className={clsx('', {
+            'mt-[-2rem]': noSpacing,
+          })}
+          loop
+          controls
+          url={external ? src : require(`@/video/${src}`).default}
+          width="100%"
+          height={external ? '423px' : '100%'}
+          volume={muted ? '0' : '0.3'}
+        />
+      </div>
     </div>
   )
 }
