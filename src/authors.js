14e0f7dd241b538c035220d45ad6651323cf92c6
0xdevelopereth <0xdevelopereth@gmail.com>
2021-12-28 16:23:09 +0100

    :hammer: relocated authors file

--- src/authors.js
+++
@@ -1,7 +0,0 @@
-import developer from './img/avatars/0xdeveloper.jpg'
-
-export const adamwathan = {
-  name: 'DΞVELOPER',
-  twitter: '0xDeveloperEth',
-  avatar: developer,
-}
---
+++ src/authors/authorList.js
@@ -0,0 +1,17 @@
+export const developer = {
+  name: 'DΞVELOPER',
+  twitter: '0xDeveloperEth',
+  avatar: require('@/img/avatars/0xdeveloper.jpg').default,
+}
+
+export const f0xboss = {
+  name: 'f0xboss',
+  twitter: 'f0xboss',
+  avatar: require('@/img/avatars/f0xboss.jpg').default,
+}
+
+export const seba = {
+  name: '0xseba',
+  twitter: '0xseba',
+  avatar: require('@/img/avatars/0xseba.jpg').default,
+}
